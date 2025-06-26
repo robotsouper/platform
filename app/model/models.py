082273579220d2938406ef96dd12e79c4dc814e0
robotsouper <qingyuan@ad.unc.edu>
2025-06-26 01:49:20 -0400

    updated model/models.py

---
+++ app/model/models.py
@@ -0,0 +1,66 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text)
+
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+
+
+class Follow(db.Model):
+    follow_id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    followed_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Post(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text)
+    image_id = db.Column(db.Integer, db.ForeignKey('image.image_id'), nullable=True)
+
+    comments = db.relationship('Comment', backref='post', lazy=True)
+    likes = db.relationship('LikePost', backref='post', lazy=True)
+    images = db.relationship('Image', backref='post', lazy=True)
+
+
+class Image(db.Model):
+    image_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable=False)
+    image_url = db.Column(db.Text, nullable=False)
+
+
+class LikePost(db.Model):
+    like_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    __table_args__ = (db.UniqueConstraint('post_id', 'user_id', name='unique_post_like'),)
+
+
+class Comment(db.Model):
+    comment_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable=False)
+    parent_comment_id = db.Column(db.Integer, db.ForeignKey('comment.comment_id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text)
+
+    replies = db.relationship('Comment', backref=db.backref('parent', remote_side=[comment_id]), lazy=True)
+    likes = db.relationship('LikeComment', backref='comment', lazy=True)
+
+
+class LikeComment(db.Model):
+    like_id = db.Column(db.Integer, primary_key=True)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comment.comment_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    __table_args__ = (db.UniqueConstraint('comment_id', 'user_id', name='unique_comment_like'),)
