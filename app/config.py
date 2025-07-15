a3dc6042b570a50667c5cbeb8f9fbd4c0c66500c
robotsouper <100224476+robotsouper@users.noreply.github.com>
2025-07-15 17:06:59 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,1 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Shuai233@localhost:5432/mydb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "super-strong-and-random-secret-key"
+
